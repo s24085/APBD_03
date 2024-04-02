7289f70c147eef0601391418ee049a73069aa286
s24085 <s24085@pjwstk.edu.pl>
2024-04-02 21:57:21 +0200

    Name walidation tests

--- zadanie/LegacyAppTests/UnitTest1.cs
+++
@@ -1,9 +0,0 @@
-namespace LegacyAppTests;
-
-public class UnitTest1
-{
-    [Fact]
-    public void Test1()
-    {
-    }
-}
---
+++ zadanie/LegacyAppTests/UserServiceTests.cs
@@ -0,0 +1,72 @@
+using LegacyApp;
+
+namespace LegacyAppTests;
+
+public class UserServiceTests
+{
+    [Fact]
+    public void AddUser_Should_Return_False_When_Email_Without_At_And_Dot()
+    {
+        //Arrange
+        string firstNAme = "John";
+        string lastName = "Doe";
+        DateTime dob = new DateTime(1997, 2, 3);
+        int clientId = 1;
+        string email = "jdoe";
+        var service = new UserService();
+        //Act
+        bool result = service.AddUser(firstNAme, lastName, email, dob, clientId);
+        //Asset
+        Assert.Equal(false, result);
+
+    }
+
+    [Fact]
+    public void AddUser_Should_Return_False_When_FirstName_Empty()
+    {
+        string firstNAme = "";
+        string lastName = "Doe";
+        DateTime dob = new DateTime(1997, 2, 3);
+        int clientId = 1;
+        string email = "jdoe";
+        var service = new UserService();
+
+        bool result = service.AddUser(firstNAme, lastName, email, dob, clientId);
+
+        Assert.Equal(false, result);
+    }
+
+    [Fact]
+    public void AddUser_Should_Return_False_When_FirstName_Null()
+    {
+
+        var service = new UserService();
+
+        bool result = service.AddUser(null, "Doe", "w@w.com", new DateTime(1990, 2, 3), 1);
+
+        Assert.Equal(false, result);
+    }
+    [Fact]
+    public void AddUser_Should_Return_False_When_LastName_Empty()
+    {
+        string firstNAme = "John";
+        string lastName = "";
+        DateTime dob = new DateTime(1997, 2, 3);
+        int clientId = 1;
+        string email = "jdoe";
+        var service = new UserService();
+
+        bool result = service.AddUser(firstNAme, lastName, email, dob, clientId);
+
+        Assert.Equal(false, result);
+    }
+    [Fact]
+    public void AddUser_Should_Return_False_When_LastName_Null()
+    {
+        var service = new UserService();
+
+        bool result = service.AddUser("John", null, "w@w.com", new DateTime(1990, 2, 3), 1);
+
+        Assert.Equal(false, result);
+    }
+}
