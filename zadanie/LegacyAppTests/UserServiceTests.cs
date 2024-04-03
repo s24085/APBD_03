878aacd772c8e245157e809c307cf52a61f713cc
s24085 <s24085@pjwstk.edu.pl>
2024-04-03 11:59:35 +0200

    Another walidation tests

--- zadanie/LegacyAppTests/UserServiceTests.cs
+++ zadanie/LegacyAppTests/UserServiceTests.cs
@@ -20,6 +20,24 @@ public class UserServiceTests
         Assert.Equal(false, result);
 
     }
+
+    [Fact]
+    public void Should_Return_True_When_After_Birthday_This_Year()
+    {
+        //Arrange
+        string firstNAme = "John";
+        string lastName = "Doe";
+        DateTime dob = new DateTime(2003, 1, 3);
+        int clientId = 1;
+        string email = "j@doe.pl";
+        var service = new UserService();
+        //Act
+        bool result = service.AddUser(firstNAme, lastName, email, dob, clientId);
+
+        Assert.Equal(true, result);
+
+    }
+
     [Fact]
     public void AddUser_Should_Return_False_When_Age_Under_21()
     {
@@ -36,6 +54,7 @@ public class UserServiceTests
         Assert.Equal(false, result);
 
     }
+
     [Fact]
     public void AddUser_Should_Return_False_When_FirstName_Empty()
     {
@@ -61,6 +80,7 @@ public class UserServiceTests
 
         Assert.Equal(false, result);
     }
+
     [Fact]
     public void AddUser_Should_Return_False_When_LastName_Empty()
     {
@@ -75,6 +95,7 @@ public class UserServiceTests
 
         Assert.Equal(false, result);
     }
+
     [Fact]
     public void AddUser_Should_Return_False_When_LastName_Null()
     {
@@ -84,4 +105,50 @@ public class UserServiceTests
 
         Assert.Equal(false, result);
     }
+
+    [Fact]
+    public void Should_Return_True_When_Type_NormalClient()
+    {
+        var service = new UserService();
+        var result = service.AddUser("John", "Doe", "jd@wp.pl", new DateTime(1999, 01, 01), 5);
+
+        Assert.Equal(true, result);
+    }
+
+    [Fact]
+    public void Should_Return_True_When_Under500CreditLimit_And_Type_NormalClient()
+    {
+        var service = new UserService();
+        var result = service.AddUser("John", "Doe", "jd@wp.pl", new DateTime(1999, 01, 01), 1);
+
+        Assert.Equal(false, result);
+    }
+
+[Fact]
+    public void Should_Return_True_When_Type_VeryImportantClient()
+    {
+        var service = new UserService();
+        var result = service.AddUser("John", "Doe", "jd@wp.pl", new DateTime(1999, 01, 01), 2);
+
+        Assert.Equal(true, result);
+    }
+
+    [Fact]
+    public void Should_Return_True_When_Type_ImportantClient()
+    {
+        var service = new UserService();
+        var result = service.AddUser("John", "Doe", "jd@wp.pl", new DateTime(1999, 01, 01), 3);
+
+        Assert.Equal(true, result);
+    }
+
+    [Fact]
+    public void Should_Return_FalseWithNoCreditLimit()
+        {
+            var service = new UserService();
+            var result = service.AddUser("John", "Doe", "jd@wp.pl", new DateTime(1999, 01, 01), 6);
+
+            Assert.Equal(true, result);
+        }
+    
 }
